57054169f668612a95ce0b8a7ab981f36f47bd94
Alan Fernandez <alan.f@msn.com>
2021-01-07 17:37:24 -0300

    Updated cors options

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const corsOptions = {
     'http://localhost:3000',
     'http://127.0.0.1',
     'http://localhost:8000',
+    'https://gearwaves-cli.herokuapp.com',
   ],
   credentials: true,
   // exposedHeaders: ['set-cookie'],
