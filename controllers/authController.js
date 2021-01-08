9db3a22da98fb7deb5a9947bce6c9d9d1327a25b
Alan Fernandez <alan.f@msn.com>
2021-01-07 23:10:42 -0300

    Auth configuration

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,9 +20,10 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-    // secure: false,
+    // secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    secure: true,
     crossDomain: true,
+    sameSite: 'none',
   });
 
   user.password = undefined;
